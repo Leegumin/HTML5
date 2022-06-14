7771381d51b167f51a86aa08df239a4b982bea6e
Leegumin <99378759+Leegumin@users.noreply.github.com>
2022-06-14 22:14:13 +0900

    2022-06-14

---
+++ 종합실습/07. paging/03/DongSungSI/src/main/java/com/example/dongsungsi/service/FreeBoardServiceImp.java
@@ -0,0 +1,15 @@
+package com.example.dongsungsi.service;
+
+/**
+ * packageName : com.example.dongsungsi.service
+ * fileName : FreeBoardServiceImp
+ * author : gumin
+ * date : 2022-06-14
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-14         gumin          최초 생성
+ */
+public class FreeBoardServiceImp implements FreeBoardService{
+}
