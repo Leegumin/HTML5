0eadfc125cafeeb9dfb8e873822ca4193a772775
Leegumin <99378759+Leegumin@users.noreply.github.com>
2022-07-23 15:59:12 +0900

    2022-07-23

--- 배포 연습용/00_TaeGyungSI/front-end/src/http-common.js
+++ 배포 연습용/00_TaeGyungSI/front-end/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:8080/api",
   // * localhost대신 ipconfig로 나온 IPv4 주소 써줌
-  baseURL: "http://192.168.0.44:8000/api",
+  baseURL: "http://192.168.0.15:8000/api",
   headers: {
     "Content-type": "application/json"
   }
